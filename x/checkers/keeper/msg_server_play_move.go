c8646c34f83f00a6a4a03bc28a1e0a6950cd9fc5
danflo27 <florentinodc@outlook.com>
2023-11-17 16:02:20 -0500

    add ability to play moves and unit tests

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -3,15 +3,70 @@ package keeper
 import (
 	"context"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	//fetch stored game info
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.GameIndex)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	//check if player is legit
+	isBlack := storedGame.Black == msg.Creator
+	isRed := storedGame.Red == msg.Creator
+	var player rules.Player
+	if !isBlack && !isRed {
+		return nil, sdkerrors.Wrapf(types.ErrCreatorNotPlayer, "%s", msg.Creator)
+	} else if isBlack && isRed {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isBlack {
+		player = rules.BLACK_PLAYER
+	} else {
+		player = rules.RED_PLAYER
+	}
+
+	//instantiate board
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//check whose turn it is
+	if !game.TurnIs(player) {
+		return nil, sdkerrors.Wrapf(types.ErrNotPlayerTurn, "%s", player)
+	}
+
+	// properly conduct the move
+	captured, moveErr := game.Move(
+		rules.Pos{
+			X: int(msg.FromX),
+			Y: int(msg.FromY),
+		},
+		rules.Pos{
+			X: int(msg.ToX),
+			Y: int(msg.ToY),
+		},
+	)
+	if moveErr != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, moveErr.Error())
+	}
+
+	//prepare updated board to be stored
+	storedGame.Board = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	// return relevant info regarding move results
+	return &types.MsgPlayMoveResponse{
+		CapturedX: int32(captured.X),
+		CapturedY: int32(captured.Y),
+		Winner:    rules.PieceStrings[game.Winner()],
+	}, nil
 }
