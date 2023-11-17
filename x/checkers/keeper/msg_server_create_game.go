2c8f1c1b584f158cce52f5269a6f954fc0e9fde6
danflo27 <florentinodc@outlook.com>
2023-11-17 14:24:07 -0500

    message verification and handling plus unit tests

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -11,7 +13,34 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
+	// get the new games id
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
+	// create new stored game object
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+	// confirm addresses
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+	//save stored game object
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	// set stage for next game
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+	// return new ID for reference
+	return &types.MsgCreateGameResponse{
+		GameIndex: newIndex,
+	}, nil
 
-	return &types.MsgCreateGameResponse{}, nil
 }
--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,14 +1,24 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/keeper"
 	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -16,6 +26,6 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", // TODO: update with a proper value when updated
+		GameIndex: "1", // TODO: update with a proper value when updated
 	}, *createResponse)
 }
